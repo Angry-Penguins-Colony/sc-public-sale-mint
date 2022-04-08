4572b08914807149ef5062dbd06804bc0b5b95c8
TF <31354550+Theo-Farnole@users.noreply.github.com>
2022-04-08 12:00:56 +0200

    ADD WIP init test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@
 
 elrond_wasm::imports!();
 
+pub const ERR_INIT_PRICE_PER_EGG_DIFF: &str = "Price per egg length different from max per wallet";
+pub const ERR_INIT_PRICE_PER_EGG_ZERO: &str = "The price list is empty";
+pub const ERR_INIT_REDUCED_PRICE_PER_EGG_DIFF: &str =
+    "Reduced rice per egg length different from max per wallet";
+pub const ERR_INIT_REDUCED_PRICE_PER_EGG_ZERO: &str = "The reduced price list is empty";
+
 #[elrond_wasm::derive::contract]
 pub trait PublicSaleMint {
     #[init]
@@ -14,9 +20,21 @@ pub trait PublicSaleMint {
         second_whitelist_delta: u64,
         first_whitelist_delta: u64,
     ) {
+        require!(price_per_egg.len() > 0, ERR_INIT_PRICE_PER_EGG_ZERO);
+
         require!(
-            price_per_egg.len() == reduced_price_per_egg.len(),
-            "Price per egg length different from max per wallet"
+            reduced_price_per_egg.len() > 0,
+            ERR_INIT_REDUCED_PRICE_PER_EGG_ZERO
+        );
+
+        require!(
+            price_per_egg.len() == max_per_wallet as usize,
+            ERR_INIT_PRICE_PER_EGG_DIFF
+        );
+
+        require!(
+            reduced_price_per_egg.len() == max_per_wallet as usize,
+            ERR_INIT_REDUCED_PRICE_PER_EGG_DIFF
         );
 
         panic!("init not implemented");
--- tests/init_tests.rs
+++ tests/init_tests.rs
@@ -17,6 +17,8 @@ fn init() {
             10,
             10,
         );
+
+        panic!("Assert eq values of sc");
     })
     .assert_ok();
 }
@@ -33,7 +35,52 @@ fn full_prices_length_different_from_max_per_wallet() {
             0,
         );
     })
-    .assert_user_error("Price per egg length different from max per wallet");
+    .assert_user_error(public_sale_mint::ERR_INIT_PRICE_PER_EGG_DIFF);
+}
+
+#[test]
+fn reduced_prices_length_different_from_max_per_wallet() {
+    warmup_init(|sc| {
+        sc.init(
+            5,
+            ManagedVec::from(vec![1u64, 2u64, 3u64, 4u64, 5u64]),
+            ManagedVec::from(vec![1u64, 2u64, 3u64, 4u64]),
+            0,
+            0,
+            0,
+        );
+    })
+    .assert_user_error(public_sale_mint::ERR_INIT_REDUCED_PRICE_PER_EGG_DIFF);
+}
+
+#[test]
+fn full_price_equals_0() {
+    warmup_init(|sc| {
+        sc.init(
+            5,
+            ManagedVec::new(),
+            ManagedVec::from(vec![1u64, 2u64, 3u64, 4u64, 5u64]),
+            0,
+            0,
+            0,
+        );
+    })
+    .assert_user_error(public_sale_mint::ERR_INIT_PRICE_PER_EGG_ZERO);
+}
+
+#[test]
+fn reduced_price_equals_0() {
+    warmup_init(|sc| {
+        sc.init(
+            5,
+            ManagedVec::from(vec![1u64, 2u64, 3u64, 4u64]),
+            ManagedVec::new(),
+            0,
+            0,
+            0,
+        );
+    })
+    .assert_user_error(public_sale_mint::ERR_INIT_REDUCED_PRICE_PER_EGG_ZERO);
 }
 
 pub fn warmup_init(tx_fn: fn(public_sale_mint::ContractObj<TxContextRef>)) -> TxResult {
