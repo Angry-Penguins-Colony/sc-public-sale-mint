6656ca4e4b6c8f80d692af8cd6bac6e1ea0e149b
TF <31354550+Theo-Farnole@users.noreply.github.com>
2022-04-08 15:18:20 +0200

    ADD wl contains test

--- tests/whitelist_tests.rs
+++ tests/whitelist_tests.rs
@@ -10,7 +10,15 @@ fn add_to_first_whitelisted() {
     let mut setup = setup_contract(public_sale_mint::contract_obj);
 
     let user = setup.users[0].clone();
-    setup.add_to_first_whitelist(user).assert_ok();
+    setup.add_to_first_whitelist(user.clone()).assert_ok();
+
+    setup
+        .blockchain_wrapper
+        .execute_tx(&user, &setup.contract_wrapper, &rust_biguint!(0u64), |sc| {
+            let contains = sc.check_contains_first(ManagedAddress::from_address(&user));
+            assert_eq!(contains, true);
+        })
+        .assert_ok();
 }
 
 #[test]
@@ -18,7 +26,15 @@ fn add_to_second_whitelisted() {
     let mut setup = setup_contract(public_sale_mint::contract_obj);
 
     let user = setup.users[0].clone();
-    setup.add_to_second_whitelist(user).assert_ok();
+    setup.add_to_second_whitelist(user.clone()).assert_ok();
+
+    setup
+        .blockchain_wrapper
+        .execute_tx(&user, &setup.contract_wrapper, &rust_biguint!(0u64), |sc| {
+            let contains = sc.check_contains_second(ManagedAddress::from_address(&user));
+            assert_eq!(contains, true);
+        })
+        .assert_ok();
 }
 
 #[test]
@@ -57,4 +73,44 @@ fn normal_user_adding_to_second_wl() {
             },
         )
         .assert_user_error(public_sale_mint::whitelist::ERR_NOT_OWNER);
+}
+
+#[test]
+fn check_contains_first_on_not_whitelisted() {
+    let setup = setup_contract(public_sale_mint::contract_obj);
+
+    let mut blockchain_state_wrapper = setup.blockchain_wrapper;
+    let address = &setup.users[0].clone();
+
+    blockchain_state_wrapper
+        .execute_tx(
+            address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                let contains = sc.check_contains_first(ManagedAddress::from_address(&address));
+                assert_eq!(contains, false);
+            },
+        )
+        .assert_ok();
+}
+
+#[test]
+fn check_contains_second_on_not_whitelisted() {
+    let setup = setup_contract(public_sale_mint::contract_obj);
+
+    let mut blockchain_state_wrapper = setup.blockchain_wrapper;
+    let address = &setup.users[0].clone();
+
+    blockchain_state_wrapper
+        .execute_tx(
+            address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                let contains = sc.check_contains_second(ManagedAddress::from_address(&address));
+                assert_eq!(contains, false);
+            },
+        )
+        .assert_ok();
 }
